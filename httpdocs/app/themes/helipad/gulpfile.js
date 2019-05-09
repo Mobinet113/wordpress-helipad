fb0f8e337ea8bf32f859280de62bee69f984afcd
Mobinet113 <benjaminroffe@mobiusit.net>
2019-05-09 11:13:07 +0100

    Updated gulp to >4.0.0

--- httpdocs/app/themes/helipad/gulpfile.js
+++ httpdocs/app/themes/helipad/gulpfile.js
@@ -1,12 +1,11 @@
-const gulp = require('gulp');
+const {src, dest, series, watch, parallel} = require('gulp');
 const imagemin = require('gulp-imagemin');
 const cache = require('gulp-cache');
 const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
 // JS Optimisation Dependencies
 const rename = require('gulp-rename');
-const babel = require('gulp-babel');
-const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 
 // PostCSS & Sass Dependencies
@@ -15,12 +14,11 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
-const browserSync = require('browser-sync').create();
 
 /**
  * Settings
  */
-let domain = 'stg.test'; // Used for BrowserSync
+let domain = 'helipad.test'; // Used for BrowserSync
 
 
 /**
@@ -28,32 +26,35 @@ let domain = 'stg.test'; // Used for BrowserSync
  * listen out for js, scss, php, html and image
  * changes and perform respective tasks
  */
-gulp.task('watch', ['browserSync', 'sass', 'scripts'], function () {
-  gulp.watch('src/scss/**/*.scss', ['sass']);  // Watch for Sass changes
-  gulp.watch('src/js/**/*.js', ['scripts']); // Watch for JS changes
-  gulp.watch('**/*.php', browserSync.reload); // Watch for PHP changes
-  gulp.watch('**/*.html', browserSync.reload); // Watch for HTML changes
-  gulp.watch('src/media/**/*', ['images']);
-});
+
+function watchForChanges(cb) {
+  watch('src/scss/**/*.scss', compileSass);  // Watch for Sass changes
+  watch('src/js/**/*.js', compileScripts); // Watch for JS changes
+  watch('**/*.php', reloadBrowser); // Watch for PHP changes
+  watch('**/*.html', reloadBrowser); // Watch for HTML changes
+  watch('src/media/**/*', minifyImages);
+}
 
 
 /**
  * Reload the browser tab on save
  */
-gulp.task('browserSync', function () {
+function reloadBrowser(cb) {
   browserSync.init({
     baseDir: './',
     proxy: 'http://' + domain,
     host: domain,
     open: 'external'
   })
-});
+}
 
 
 /**
- * Gulp Sass compile task
+ * Gulp SASS compile task
+ * @param cb
+ * @returns {*}
  */
-gulp.task('sass', function () {
+function compileSass(cb) {
 
   // PostCss Plugins to run after Sass compile
   let postCssPlugins = [
@@ -61,7 +62,7 @@ gulp.task('sass', function () {
     cssnano()
   ];
 
-  return gulp.src('src/scss/main.scss')
+  return src('src/scss/main.scss')
     .pipe(sourcemaps.init())
 
     // We want to log any sass errors
@@ -73,17 +74,19 @@ gulp.task('sass', function () {
     // Generate source maps and write our new compiled
     // css file to the dist/css directory
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/css'))
+    .pipe(dest('dist/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
-});
+}
 
 /**
  * Gulp Javascript compile task
+ * @param cb
+ * @returns {*}
  */
-gulp.task('scripts', function () {
-  return gulp.src('src/js/index.js')
+function compileScripts(cb) {
+  return src('src/js/index.js')
     .pipe(sourcemaps.init())
 
     .pipe(webpackStream(
@@ -99,7 +102,7 @@ gulp.task('scripts', function () {
               exclude: /(node_modules)/,
               loader: 'babel-loader',
               query: {
-                presets: ['@babel/env']
+                presets: ['@babel/env', '@babel/typescript']
               }
             }
           ]
@@ -112,27 +115,42 @@ gulp.task('scripts', function () {
     .pipe(sourcemaps.write())
 
     .pipe(rename('scripts.js'))
-    .pipe(gulp.dest('dist/js/'))
+    .pipe(dest('dist/js/'))
 
     .pipe(browserSync.reload({
       stream: true
     }));
 
-});
+}
 
 /**
- * Gulp image optimisation task
+ * Gulp image minification task
+ * @param cb
+ * @returns {*}
  */
-gulp.task('images', function () {
-  return gulp.src('src/media/**/*.+(png|jpg|gif|svg)')
+function minifyImages(cb) {
+  return src('src/media/**/*.+(png|jpg|gif|svg)')
     .pipe(cache(imagemin({
       interlaced: true
     })))
-    .pipe(gulp.dest('dist/media'))
-});
+    .pipe(dest('dist/media'))
+}
 
-
+/**
+ * Output errors to the console
+ * @param err
+ */
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
+
+
+// Module exports...
+
+exports.sass = compileSass;
+exports.script = compileScripts;
+exports.images = minifyImages;
+
+exports.watch = series(reloadBrowser, watchForChanges);
+exports.default = parallel(compileSass, compileScripts, minifyImages);
--- httpdocs/app/themes/helipad/src/js/components/Masthead.js
+++ httpdocs/app/themes/helipad/src/js/components/Masthead.js
@@ -1,12 +1,8 @@
-import TweenLite from 'gsap/TweenLite';
-import $ from 'jquery';
 
 class Masthead {
   constructor() {
 
     // Configurable Vars
-    this.$masthead = $('#masthead');
-    this.minOpacity = 0.4;
 
     // Automatically defined vars
     this.scrolled = false;
@@ -24,26 +20,13 @@ class Masthead {
   getInitialPagePosition = () => {
     let scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
-    this.minifyMasthead(scrollTop);
   };
 
   _onPageScroll = evt => {
-    this.minifyMasthead(evt.pageY);
+
+
   };
 
-  minifyMasthead = amntScrolled => {
-
-    let opacity = amntScrolled / 600;
-
-    if ( opacity < this.minOpacity ){
-      opacity = this.minOpacity;
-    }
-
-    // Set the opacity of the masthead background depending on scroll distance (but don't go much further than 1!)
-    if ( opacity <= 1.1) {
-      this.tween = TweenLite.to(this.$masthead.find('.background'), 1, {opacity: opacity});
-    }
-  };
 
 
 }
