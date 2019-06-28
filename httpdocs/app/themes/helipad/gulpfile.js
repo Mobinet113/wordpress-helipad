c990a75647874550b5524877a5ef12124ea2ae0e
Mobinet113 <benjaminroffe@mobiusit.net>
2019-06-28 11:49:30 +0100

    Resolved issue with gulp build process

--- httpdocs/app/themes/helipad/gulpfile.js
+++ httpdocs/app/themes/helipad/gulpfile.js
@@ -33,125 +33,121 @@ let domain = 'helipad.test' // Used for BrowserSync.
  */
 
 function watchForChanges () {
-  watch( 'src/scss/**/*.scss', compileSass )  // Watch for Sass changes.
-  watch( '**/*.ts', compileScripts ) // Watch for JS changes.
-  watch( '**/*.php', browserSync.reload ) // Watch for PHP changes.
-  watch( '**/*.html', browserSync.reload ) // Watch for HTML changes.
-  watch( 'src/media/**/*', minifyImages )
+	watch( 'src/scss/**/*.scss', compileSass ) // Watch for Sass changes.
+	watch( '**/*.ts', compileScripts ) // Watch for JS changes.
+	watch( '**/*.php', browserSync.reload ) // Watch for PHP changes.
+	watch( '**/*.html', browserSync.reload ) // Watch for HTML changes.
+	watch( 'src/media/**/*', minifyImages )
 }
 
 /**
  * Reload the browser tab on save
  */
 function reloadBrowser () {
-  browserSync.init( {
-                      baseDir: './',
-                      proxy: 'http://' + domain,
-                      host: domain,
-                      open: 'external',
-                    } )
+	browserSync.init(
+		{
+			baseDir: './',
+			proxy: 'http://' + domain,
+			host: domain,
+			open: 'external',
+		},
+	)
 }
 
 /**
  * Gulp SASS compile task
+ *
  * @returns {*}
  */
 function compileSass () {
 
-  // PostCss Plugins to run after Sass compile.
-  let postCssPlugins = [
-    autoprefixer(),
-    cssnano(),
-  ]
+	// PostCss Plugins to run after Sass compile.
+	let postCssPlugins = [
+		autoprefixer(),
+		cssnano(),
+	]
 
-  return src( 'src/scss/main.scss' ).
-    pipe( sourcemaps.init() )
+	return src( 'src/scss/main.scss' )
+		.pipe( sourcemaps.init() )
 
-    // We want to log any sass errors
-    .pipe( sass().on( 'error', sass.logError ) )
+		// We want to log any sass errors.
+		.pipe( sass().on( 'error', sass.logError ) )
 
-    // PostCSS Pipeline
-    .pipe( postcss( postCssPlugins ) )
+		// PostCSS Pipeline.
+		.pipe( postcss( postCssPlugins ) )
 
-    // Generate source maps and write our new compiled
-    // css file to the dist/css directory
-    .pipe( sourcemaps.write() ).
-    pipe( dest( 'dist/css' ) ).
-    pipe( browserSync.reload( {
-                                stream: true,
-                              } ) )
+		// Generate source maps and write our new compiled
+		// css file to the dist/css directory.
+		.pipe( sourcemaps.write() )
+		.pipe( dest( 'dist/css' ) )
+		.pipe( browserSync.reload( { stream: true } ) )
 }
 
 /**
  * Gulp Javascript compile task
- * @param cb
+ *
  * @returns {*}
  */
 function compileScripts () {
-  return src( 'src/js/index.ts' ).
-    pipe( sourcemaps.init() ).
-    pipe( webpackStream(
-      {
-        mode: 'development',
-        entry: './src/js/index.ts',
-        output: {
-          filename: 'scripts.js',
-        },
-        resolve: {
-          // Add `.ts` and `.tsx` as a resolvable extension.
-          extensions: [ '.ts', '.tsx', '.js' ],
-        },
-        module: {
-          rules: [
-            {
-              test: /\.(ts)$/,
-              exclude: /(node_modules)/,
-              loader: 'ts-loader',
-            },
-          ],
-        },
-      },
-    ) ).
-    on( 'error', handleError ).
-    pipe( rename( 'scripts.js' ) ).
-    pipe( dest( 'dist/js/' ) ).
-    pipe( browserSync.reload( {
-                                stream: true,
-                              } ) )
+	return src( 'src/js/index.ts' ).pipe( sourcemaps.init() )
+		.pipe(
+			webpackStream(
+				{
+					mode: 'development',
+					entry: './src/js/index.ts',
+					output: {
+						filename: 'scripts.js',
+					},
+					resolve: {
+						// Add `.ts` and `.tsx` as a resolvable extension.
+						extensions: [ '.ts', '.tsx', '.js' ],
+					},
+					module: {
+						rules: [
+							{
+								test: /\.(ts)$/,
+								exclude: /(node_modules)/,
+								loader: 'ts-loader',
+							},
+						],
+					},
+				},
+			),
+		)
+		.on( 'error', handleError )
+		.pipe( rename( 'scripts.js' ) )
+		.pipe( dest( 'dist/js/' ) )
+		.pipe( browserSync.reload( { stream: true } ) )
 
 }
 
 /**
  * Gulp image minification task
+ *
  * @returns {*}
  */
 function minifyImages () {
-  return src( 'src/media/**/*.+(png|jpg|gif|svg)' ).
-    pipe( cache( imagemin( {
-                             interlaced: true,
-                           } ) ) ).
-    pipe( dest( 'dist/media' ) )
+	return src( 'src/media/**/*.+(png|jpg|gif|svg)' )
+		.pipe(
+			cache(
+				imagemin(
+					{
+						interlaced: true,
+					},
+				),
+			),
+		)
+		.pipe( dest( 'dist/media' ) )
 }
 
 /**
  * Output errors to the console
+ *
  * @param err
  */
 function handleError ( err ) {
-  console.log( err.toString() )
-  this.emit( 'end' )
-}
-
-/**
- * Run compilation and minification functions
- * @returns {*}
- */
-function build () {
-  return parallel(
-    compileScripts,
-    compileSass,
-    minifyImages,
-  )
+	console.log( err.toString() )
+	this.emit( 'end' )
 }
 
 // Module exports...
@@ -159,8 +155,9 @@ function build () {
 exports.sass = compileSass
 exports.scripts = compileScripts
 exports.images = minifyImages
-
 exports.watch = parallel( reloadBrowser, watchForChanges )
-
-exports.build = build
-exports.default = build
+exports.build = parallel(
+	compileScripts,
+	compileSass,
+	minifyImages,
+)
